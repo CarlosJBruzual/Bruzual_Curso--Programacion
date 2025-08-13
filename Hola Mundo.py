6c953f2a965432ccd182909d015a389de91afafc
CarlosJBruzual <carlosbruzual@rivasdev.com>
2025-08-13 11:14:09 -0400

    a

---
+++ Hola Mundo.py
@@ -0,0 +1,20 @@
+
+
+print("Ingresa la calificacion del alumno")
+calificacion = float(input("calificación: "))
+if calificacion <= 59:    
+    print("La clificacion es F")
+elif calificacion >= 60 and calificacion <= 69:
+    print("La calificacion es D")
+elif calificacion >= 70 and calificacion <= 79:
+    print("La calificacion es C")
+elif calificacion >= 80 and calificacion <= 89:
+    print("La calificacion es B")
+elif calificacion >= 90 and calificacion <= 100:
+    print("La calificacion es A")
+else:
+    print("Calificación no válida")
+
+
+
+
