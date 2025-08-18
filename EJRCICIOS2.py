bfdffa3d537e7a301b17d4662cd01fb7a5f19f12
CarlosJBruzual <carlosbruzual@rivasdev.com>
2025-08-18 16:09:42 -0400

    v1

---
+++ EJRCICIOS2.py
@@ -0,0 +1,7 @@
+import random
+ramdonumeroaleatorio = random.randint(1, 100)
+print(f"El número aleatorio generado es: {ramdonumeroaleatorio}")
+
+import math
+numero = math.sqrt(16)
+print(f"La raíz cuadrada de 16 es: {numero}")
